517a5add78add92134816003ee060b39938f34a8
Rohit Chaudhary <40576340+rorochaudhary@users.noreply.github.com>
2020-08-18 15:54:24 -0700

    uploading project

---
+++ hash_map.py
@@ -0,0 +1,392 @@
+# Course: CS261 - Data Structures
+# Assignment: 5 - Hash Map Implementation
+# Student: Rohit Chaudhary
+# Description: Hash map implementation using a DynamicArray and LinkedList to handle hash collisions. Methods of put, get, remove, contains_key, clear, empty_buckets, resize_table, table_load, get_keys are implemented.
+
+
+# Import pre-written DynamicArray and LinkedList classes
+from a5_include import *
+
+
+def hash_function_1(key: str) -> int:
+    """
+    Sample Hash function #1 to be used with A5 HashMap implementation
+    """
+    hash = 0
+    for letter in key:
+        hash += ord(letter)
+    return hash
+
+
+def hash_function_2(key: str) -> int:
+    """
+    Sample Hash function #2 to be used with A5 HashMap implementation
+    """
+    hash, index = 0, 0
+    index = 0
+    for letter in key:
+        hash += (index + 1) * ord(letter)
+        index += 1
+    return hash
+
+
+class HashMap:
+    def __init__(self, capacity: int, function) -> None:
+        """
+        Init new HashMap based on DA with SLL for collision resolution
+        """
+        self.buckets = DynamicArray()
+        for _ in range(capacity):
+            self.buckets.append(LinkedList())
+        self.capacity = capacity
+        self.hash_function = function
+        self.size = 0
+
+    def __str__(self) -> str:
+        """
+        Return content of hash map t in human-readable form
+        """
+        out = ''
+        for i in range(self.buckets.length()):
+            list = self.buckets.get_at_index(i)
+            out += str(i) + ': ' + str(list) + '\n'
+        return out
+
+    def clear(self) -> None:
+        """
+        function clears all contents of the hash map, current capacity of the hash table is not affected.
+        """
+        # replace current DynamicArray with a new one of same capacity
+        new_table = DynamicArray()
+        for i in range(self.capacity):
+            new_table.append(LinkedList())
+        self.bucket = new_table
+        self.size = 0
+        return
+
+    def get(self, key: str) -> object:
+        """
+        function returns the value associated with key. If key is not in the hash map, function returns None.
+        """
+        hash_index = self.hash_function(key) % self.capacity
+        bucket = self.buckets.get_at_index(hash_index)
+        # empty bucket
+        if bucket.length() == 0:
+            return None
+        elif bucket.contains(key) != None:  # found key    
+            return bucket.contains(key).value
+        else:   # key not found in expected chain
+            return None
+
+    def put(self, key: str, value: object) -> None:
+        """
+        function adds the new key:value pair into the hash table. If the key:value pair already exists the hash table, the current value is overwritten with the new value.
+        """
+        hash_index = self.hash_function(key) % self.capacity
+        bucket = self.buckets.get_at_index(hash_index)
+
+        # empty bucket --> add directly
+        if bucket.length() == 0:
+            bucket.insert(key, value)
+            self.size += 1
+        # non empty bucket --> check for key
+        else:
+            if bucket.contains(key): # key present, remove and replace bucket item
+                bucket.contains(key).value = value
+            else:
+                bucket.insert(key, value)
+                self.size += 1
+
+        return
+
+    def remove(self, key: str) -> None:
+        """
+        function removes key and its value from the hash map, otherwise does nothing.
+        """
+        hash_index = self.hash_function(key) % self.capacity
+        bucket = self.buckets.get_at_index(hash_index)
+        if bucket.contains(key) != None:  # found key    
+            bucket.remove(key)
+            self.size -= 1
+
+    def contains_key(self, key: str) -> bool:
+        """
+        function returns True if key is within the hash map, False otherwise. empty hash table will have no keys, returns False.
+        """
+        hash_index = self.hash_function(key) % self.capacity
+        bucket = self.buckets.get_at_index(hash_index)
+        if bucket.length() == 0:
+            return False
+        elif bucket.contains(key) != None:
+            return True
+        else:
+            return False
+
+    def empty_buckets(self) -> int:
+        """
+        function returns the number of empty buckets in the table.
+        """
+        empty = 0
+        # if LinkedList chain length is 0, bucket is empty
+        for i in range(self.capacity):
+            if self.buckets.get_at_index(i).length() == 0:
+                empty += 1
+        return empty
+
+    def table_load(self) -> float:
+        """
+        function returns the current load factor of the hash table.
+        """
+        # lambda = n / m, where n = number of elements and m = number of buckets
+        load = self.size / self.capacity
+        return load
+
+    def resize_table(self, new_capacity: int) -> None:
+        """
+        function changes the internal capacity of the hash map to new_capacity. All current key:value pairs remain after function completes. new_capacity less than 1 does nothing.
+        """
+        if new_capacity < 1:
+            return
+        else:
+            # create a new hashmap that will receive old hash values
+            new_map = HashMap(new_capacity, self.hash_function)
+
+            # go through old map and re-hash everything into new map
+            for i in range(self.capacity):
+                bucket = self.buckets.get_at_index(i)
+                # iterate over chains (containing key-value nodes)
+                for pair in bucket: 
+                    key, value = pair.key, pair.value
+                    new_map.put(key, value) # hash into new map
+            
+            # replace old map with new map
+            self.buckets = new_map.buckets
+            self.capacity = new_map.capacity
+            return
+                
+
+    def get_keys(self) -> DynamicArray:
+        """
+        function returns a DynamicArray instance containing all the keys stored in the current hash map.
+        """
+        keys = DynamicArray()
+        # iterate over the buckets
+        for i in range(self.capacity):
+            bucket = self.buckets.get_at_index(i)
+            # iterate over each chain, getting key
+            for item in bucket:
+                keys.append(item.key)
+
+        return keys
+
+
+# BASIC TESTING
+# if __name__ == "__main__":
+
+    # # my own testing for written questions
+    # print("--------hash func 1------------")
+    # m = HashMap(50, hash_function_1)
+    # for i in range(50):
+    #     m.put('key' + str(i), i * 100)
+    #     if i % 10 == 0:
+    #         print(m.table_load(), m.size, m.capacity, m.empty_buckets())
+    # print("---------hash func 2----------")
+    # m = HashMap(50, hash_function_2)
+    # for i in range(50):
+    #     m.put('key' + str(i), i * 100)
+    #     if i % 10 == 0:
+    #         print(m.table_load(), m.size, m.capacity, m.empty_buckets())
+    # print("--------------------------")
+
+    # print("\nPDF - empty_buckets example 1")
+    # print("-----------------------------")
+    # m = HashMap(100, hash_function_1)
+    # print(m.empty_buckets(), m.size, m.capacity)
+    # m.put('key1', 10)
+    # print(m.empty_buckets(), m.size, m.capacity)
+    # m.put('key2', 20)
+    # print(m.empty_buckets(), m.size, m.capacity)
+    # m.put('key1', 30)
+    # print(m.empty_buckets(), m.size, m.capacity)
+    # m.put('key4', 40)
+    # print(m.empty_buckets(), m.size, m.capacity)
+
+
+    # print("\nPDF - empty_buckets example 2")
+    # print("-----------------------------")
+    # m = HashMap(50, hash_function_1)
+    # for i in range(150):
+    #     m.put('key' + str(i), i * 100)
+    #     if i % 30 == 0:
+    #         print(m.empty_buckets(), m.size, m.capacity)
+
+
+    # print("\nPDF - table_load example 1")
+    # print("--------------------------")
+    # m = HashMap(100, hash_function_1)
+    # print(m.table_load())
+    # m.put('key1', 10)
+    # print(m.table_load())
+    # m.put('key2', 20)
+    # print(m.table_load())
+    # m.put('key1', 30)
+    # print(m.table_load())
+
+
+    # print("\nPDF - table_load example 2")
+    # print("--------------------------")
+    # m = HashMap(50, hash_function_1)
+    # for i in range(50):
+    #     m.put('key' + str(i), i * 100)
+    #     if i % 10 == 0:
+    #         print(m.table_load(), m.size, m.capacity)
+
+    # print("\nPDF - clear example 1")
+    # print("---------------------")
+    # m = HashMap(100, hash_function_1)
+    # print(m.size, m.capacity)
+    # m.put('key1', 10)
+    # m.put('key2', 20)
+    # m.put('key1', 30)
+    # print(m.size, m.capacity)
+    # m.clear()
+    # print(m.size, m.capacity)
+
+
+    # print("\nPDF - clear example 2")
+    # print("---------------------")
+    # m = HashMap(50, hash_function_1)
+    # print(m.size, m.capacity)
+    # m.put('key1', 10)
+    # print(m.size, m.capacity)
+    # m.put('key2', 20)
+    # print(m.size, m.capacity)
+    # m.resize_table(100)
+    # print(m.size, m.capacity)
+    # m.clear()
+    # print(m.size, m.capacity)
+
+
+    # print("\nPDF - put example 1")
+    # print("-------------------")
+    # m = HashMap(50, hash_function_1)
+    # for i in range(150):
+    #     m.put('str' + str(i), i * 100)
+    #     if i % 25 == 24:
+    #         print(m.empty_buckets(), m.table_load(), m.size, m.capacity)
+
+
+    # print("\nPDF - put example 2")
+    # print("-------------------")
+    # m = HashMap(40, hash_function_2)
+    # for i in range(50):
+    #     m.put('str' + str(i // 3), i * 100)
+    #     if i % 10 == 9:
+    #         print(m.empty_buckets(), m.table_load(), m.size, m.capacity)
+
+
+    # print("\nPDF - contains_key example 1")
+    # print("----------------------------")
+    # m = HashMap(10, hash_function_1)
+    # print(m.contains_key('key1'))
+    # m.put('key1', 10)
+    # m.put('key2', 20)
+    # m.put('key3', 30)
+    # print(m.contains_key('key1'))
+    # print(m.contains_key('key4'))
+    # print(m.contains_key('key2'))
+    # print(m.contains_key('key3'))
+    # m.remove('key3')
+    # print(m.contains_key('key3'))
+
+
+    # print("\nPDF - contains_key example 2")
+    # print("----------------------------")
+    # m = HashMap(75, hash_function_2)
+    # keys = [i for i in range(1, 1000, 20)]
+    # for key in keys:
+    #     m.put(str(key), key * 42)
+    # print(m.size, m.capacity)
+    # result = True
+    # for key in keys:
+    #     # all inserted keys must be present
+    #     result &= m.contains_key(str(key))
+    #     # NOT inserted keys must be absent
+    #     result &= not m.contains_key(str(key + 1))
+    # print(result)
+
+
+    # print("\nPDF - get example 1")
+    # print("-------------------")
+    # m = HashMap(30, hash_function_1)
+    # print(m.get('key'))
+    # m.put('key1', 10)
+    # print(m.get('key1'))
+
+
+    # print("\nPDF - get example 2")
+    # print("-------------------")
+    # m = HashMap(150, hash_function_2)
+    # for i in range(200, 300, 7):
+    #     m.put(str(i), i * 10)
+    # print(m.size, m.capacity)
+    # for i in range(200, 300, 21):
+    #     print(i, m.get(str(i)), m.get(str(i)) == i * 10)
+    #     print(i + 1, m.get(str(i + 1)), m.get(str(i + 1)) == (i + 1) * 10)
+
+
+    # print("\nPDF - remove example 1")
+    # print("----------------------")
+    # m = HashMap(50, hash_function_1)
+    # print(m.get('key1'))
+    # m.put('key1', 10)
+    # print(m.get('key1'))
+    # m.remove('key1')
+    # print(m.get('key1'))
+    # m.remove('key4')
+
+
+    # print("\nPDF - resize example 1")
+    # print("----------------------")
+    # m = HashMap(20, hash_function_1)
+    # m.put('key1', 10)
+    # print(m.size, m.capacity, m.get('key1'), m.contains_key('key1'))
+    # m.resize_table(30)
+    # print(m.size, m.capacity, m.get('key1'), m.contains_key('key1'))
+
+
+    # print("\nPDF - resize example 2")
+    # print("----------------------")
+    # m = HashMap(75, hash_function_2)
+    # keys = [i for i in range(1, 1000, 13)]
+    # for key in keys:
+    #     m.put(str(key), key * 42)
+    # print(m.size, m.capacity)
+
+    # for capacity in range(111, 1000, 117):
+    #     m.resize_table(capacity)
+
+    #     m.put('some key', 'some value')
+    #     result = m.contains_key('some key')
+    #     m.remove('some key')
+
+    #     for key in keys:
+    #         result &= m.contains_key(str(key))
+    #         result &= not m.contains_key(str(key + 1))
+    #     print(capacity, result, m.size, m.capacity, round(m.table_load(), 2))
+
+
+    # print("\nPDF - get_keys example 1")
+    # print("------------------------")
+    # m = HashMap(10, hash_function_2)
+    # for i in range(100, 200, 10):
+    #     m.put(str(i), str(i * 10))
+    # print(m.get_keys())
+
+    # m.resize_table(1)
+    # print(m.get_keys())
+
+    # m.put('200', '2000')
+    # m.remove('100')
+    # m.resize_table(2)
+    # print(m.get_keys())
---
+++ min_heap.py
@@ -0,0 +1,266 @@
+# Course: CS261 - Data Structures
+# Assignment: 5 - Min Heap Implementation
+# Student: Rohit Chaudhary
+# Description: Implementation of a Min Heap with methods of add, get_min, remove_min, and build_heap. Utilizes DynamicArray as underlying data structure.
+
+
+# Import pre-written DynamicArray and LinkedList classes
+from a5_include import *
+
+
+class MinHeapException(Exception):
+    """
+    Custom exception to be used by MinHeap class
+    DO NOT CHANGE THIS CLASS IN ANY WAY
+    """
+    pass
+
+
+class MinHeap:
+    def __init__(self, start_heap=None):
+        """
+        Initializes a new MinHeap
+        """
+        self.heap = DynamicArray()
+
+        # populate MH with initial values (if provided)
+        # before using this feature, implement add() method
+        if start_heap:
+            for node in start_heap:
+                self.add(node)
+
+    def __str__(self) -> str:
+        """
+        Return MH content in human-readable form
+        """
+        return 'HEAP ' + str(self.heap)
+
+    def is_empty(self) -> bool:
+        """
+        Return True if no elements in the heap, False otherwise
+        """
+        return self.heap.length() == 0
+
+    def add(self, node: object) -> None:
+        """
+        function adds a new value to the heap, maintaining heap property.
+        """
+        # add item to heap and percolate to correct spot
+        self.heap.append(node)
+        self.percolate_up()
+
+        return
+
+    def percolate_up(self) -> None:
+        """
+        function is a helper method to add() that will "percolate" the last heap item to its correct heap position with repeated swaps.
+        """
+        # starting node and parent values
+        node_index = self.heap.length() - 1
+        parent_index = (node_index - 1) // 2
+        node = self.heap.get_at_index(node_index)
+        parent = self.heap.get_at_index(parent_index)
+
+        # while parent > node
+        while parent > node:
+            # if node is a the top (index 0), we are done
+            if node_index == 0:
+                break
+            # swap node and parent
+            else:
+                self.heap.swap(node_index, parent_index)
+                # recalculate node and parent
+                node_index = parent_index
+                parent_index = (node_index - 1) // 2
+                node = self.heap.get_at_index(node_index)
+                parent = self.heap.get_at_index(parent_index)
+        
+        return
+
+    def get_min(self) -> object:
+        """
+        function returns the top value of the MinHeap. raises MinHeapException if heap is empty
+        """
+        if self.is_empty():
+            raise MinHeapException
+        else:
+            return self.heap.get_at_index(0)
+
+    def remove_min(self) -> object:
+        """
+        function removes the minimum key element from the heap, returning the value. removing from empty heap raises MinHeapException.
+        """
+        if self.is_empty():
+            raise MinHeapException
+        else:
+            min_element = self.get_min()
+            # swap last and first heap elements, removing the last
+            self.heap.swap(0, self.heap.length() - 1)
+            self.heap.pop()
+            # check if removed the only element (empty heap)
+            if not self.is_empty():
+                # move element to proper position within heap
+                self.percolate_down()
+    
+            return min_element
+
+    def percolate_down(self) -> None:
+        """
+        function is a helper method to remove_min(), "percolating" the top heap element downwards via repeated swaps, maintaining min-heap property.
+        """
+        # indices
+        i = 0
+        i_left = (2 * i) + 1
+        i_right = (2 * i) + 2
+        i_small = 0
+
+        # keep swapping while there are valid indices
+        while i_left < self.heap.length() or i_right < self.heap.length():
+            # current node and its children
+            current = self.heap.get_at_index(i)
+
+            # handle one or two children
+            if i_left >= self.heap.length():
+                right_child = self.heap.get_at_index(i_right)
+                smaller_child = right_child
+                i_small = i_right
+            elif i_right >= self.heap.length():
+                left_child = self.heap.get_at_index(i_left)
+                smaller_child = left_child
+                i_small = i_left
+            else:   # compare children to find the smaller
+                right_child = self.heap.get_at_index(i_right)
+                left_child = self.heap.get_at_index(i_left)
+                if left_child <= right_child:
+                    smaller_child = left_child
+                    i_small = i_left
+                else:
+                    smaller_child = right_child
+                    i_small = i_right
+            # found smaller child, compare with current
+            if current > smaller_child:
+                # swap
+                self.heap.swap(i, i_small)
+
+                # recalculate indices
+                i = i_small
+                i_left = (2 * i) + 1
+                i_right = (2 * i) + 2
+                i_small = 0
+
+            else:
+                break
+        return
+
+    def build_heap(self, da: DynamicArray) -> None:
+        """
+        function recieves a DynamicArray with objects in any order and builds a valid MinHeap
+        """
+        # copy heap into new_heap
+        new_heap = DynamicArray()
+        for j in range(da.length()):
+            new_heap.append(da.get_at_index(j))
+
+        # get the first non-leaf index
+        i = (new_heap.length() // 2) - 1
+        for k in range(i + 1, -1, -1):
+            self.percolate_build(new_heap, k)
+
+        # new_heap is sorted, make that the main heap
+        self.heap = new_heap
+        return
+
+    def percolate_build(self, da, non_leaf_index) -> None:
+        """
+        function is a helper method to build_heap, which percolates down the DynArray (da) heap element at position non_leaf_index into proper position. similar to percolate_down helper method implemented for remove_min.
+        """
+        # indices
+        i = non_leaf_index
+        i_left = (2 * i) + 1
+        i_right = (2 * i) + 2
+        i_small = 0
+
+        # keep swapping while there are valid indices
+        while i_left < da.length() or i_right < da.length():
+            # current node and its children
+            current = da.get_at_index(i)
+
+            # handle one or two children
+            if i_left >= da.length():
+                right_child = da.get_at_index(i_right)
+                smaller_child = right_child
+                i_small = i_right
+            elif i_right >= da.length():
+                left_child = da.get_at_index(i_left)
+                smaller_child = left_child
+                i_small = i_left
+            else:   # compare children to find the smaller
+                right_child = da.get_at_index(i_right)
+                left_child = da.get_at_index(i_left)
+                if left_child <= right_child:
+                    smaller_child = left_child
+                    i_small = i_left
+                else:
+                    smaller_child = right_child
+                    i_small = i_right
+            # found smaller child, compare with current
+            if current > smaller_child:
+                # swap
+                da.swap(i, i_small)
+
+                # recalculate indices
+                i = i_small
+                i_left = (2 * i) + 1
+                i_right = (2 * i) + 2
+                i_small = 0
+
+            else:
+                break
+        
+        return
+
+# BASIC TESTING
+# if __name__ == '__main__':
+
+    # print("\nPDF - add example 1")
+    # print("-------------------")
+    # h = MinHeap()
+    # print(h, h.is_empty())
+    # for value in range(300, 200, -15):
+    #     h.add(value)
+    #     print(h)
+
+    # print("\nPDF - add example 2")
+    # print("-------------------")
+    # h = MinHeap(['fish', 'bird'])
+    # print(h)
+    # for value in ['monkey', 'zebra', 'elephant', 'horse', 'bear']:
+    #     h.add(value)
+    #     print(h)
+
+
+    # print("\nPDF - get_min example 1")
+    # print("-----------------------")
+    # h = MinHeap(['fish', 'bird'])
+    # print(h)
+    # print(h.get_min(), h.get_min())
+
+
+    # print("\nPDF - remove_min example 1")
+    # print("--------------------------")
+    # h = MinHeap([1, 10, 2, 9, 3, 8, 4, 7, 5, 6])
+    # while not h.is_empty():
+    #     print(h, end=' ')
+    #     print(h.remove_min())
+
+
+    # print("\nPDF - build_heap example 1")
+    # print("--------------------------")
+    # da = DynamicArray([100, 20, 6, 200, 90, 150, 300])
+    # h = MinHeap(['zebra', 'apple'])
+    # print(h)
+    # h.build_heap(da)
+    # print(h)
+    # da.set_at_index(0, 500)
+    # print(da)
+    # print(h)
